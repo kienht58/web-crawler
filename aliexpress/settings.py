6f836756b2665a04926e37ca9cd793f0a6f98640
kienht58 <kien.isp15@gmail.com>
2016-10-23 23:02:13 +0700

    almost finished

--- aliexpress/settings.py
+++ aliexpress/settings.py
@@ -22,19 +22,22 @@ NEWSPIDER_MODULE = 'aliexpress.spiders'
 ROBOTSTXT_OBEY = False
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
-#CONCURRENT_REQUESTS = 32
+CONCURRENT_REQUESTS = 10000
 
 # Configure a delay for requests for the same website (default: 0)
 # See http://scrapy.readthedocs.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
-#DOWNLOAD_DELAY = 3
+# DOWNLOAD_DELAY = 1
 # The download delay setting will honor only one of:
 #CONCURRENT_REQUESTS_PER_DOMAIN = 16
 #CONCURRENT_REQUESTS_PER_IP = 16
 
 # Disable cookies (enabled by default)
-#COOKIES_ENABLED = False
+COOKIES_ENABLED = True
 
+# DEPTH_PRIORITY = 1
+# SCHEDULER_DISK_QUEUE = 'scrapy.squeues.PickleFifoDiskQueue'
+# SCHEDULER_MEMORY_QUEUE = 'scrapy.squeues.FifoMemoryQueue'
 # Disable Telnet Console (enabled by default)
 #TELNETCONSOLE_ENABLED = False
 
--- aliexpress/spiders/productSpider.py
+++ aliexpress/spiders/productSpider.py
@@ -12,25 +12,24 @@ import json
 class ProductSpider(InitSpider):
     name = 'product'
     login_page = "https://login.aliexpress.com"
+    buffer_url = "https://www.aliexpress.com/"
     start_url = "https://www.aliexpress.com/category/200004720/office-electronics.html?spm=2114.20011208.2.1.AkTOEP&site=glo"
     products = {}
 
     def __init__(self):
         InitSpider.__init__(self)
+        self.products['iteration'] = 0
         self.driver = webdriver.Chrome()
 
     def __del__(self):
         self.driver.close()
-        print self.verificationErrors
         CrawlSpider.__del__(self)
 
     def init_request(self):
         #create login request
-        self.log('[LOG]: Start crawling login page!')
         return Request(url=self.login_page, callback=self.login)
 
     def login(self, response):
-        self.log('[LOG]: Get iframe and fill in login form.')
         self.driver.get(response.url)
         #return login page as response
         frame = self.driver.find_element_by_id('alibaba-login-box')
@@ -45,38 +44,61 @@ class ProductSpider(InitSpider):
 
         #submit
         self.driver.find_element_by_name("submit-btn").click()
-
         return Request(url=self.start_url, callback=self.get_list_products)
 
     def get_list_products(self, response):
-        if "Sign Out" in response.body:
-            #approved
-            self.log("[LOG]: Successfully logged in. Start crawling!")
-            product_urls = response.css("a.product::attr(href)").extract()
-            for product_url in product_urls:
-                product_url = product_url[2:]
-                product_id = product_url.split("/")[3].split("?")[0].replace(".html", "")
-                self.log(product_id)
-                products[product_id] = Request(url=product_url, callback=self.parse)
-        else:
-            self.log("[LOG]: Failed!")
+        if "login" in response.url:
+            self.driver.get(response.url)
+            validation_box = self.driver.find_element_by_id('alibaba-login-box')
+            self.driver.switch_to.frame(validation_box)
+            self.driver.find_element_by_id('has-login-submit').click()
 
-    def parse(self, response):
-        product = {}
-        product['name'] = response.css("h1.product-name::text").extract_first()
-        product['orders'] = response.css("span.order-num::text").extract_first().replace('orders', '').strip()
-        us_orders = 0
-        for index in range((orders // 8) if (orders % 8 == 0) else (orders // 8 + 1)):
-            us_orders_per_page =  Request(url="https://feedback.aliexpress.com/display/evaluationProductDetailAjaxService.htm?productId=" + product_id + "&type=default&page=" + str(index + 1), callback=self.get_us_order)
-            us_orders = us_orders + us_orders_per_page
-        product['us_orders'] = us_orders
-        return product
+            self.driver.get(self.start_url)
+            products = self.driver.find_elements_by_class_name("list-item")
+            for product in products:
+                product_url = product.find_element_by_class_name("product").get_attribute("href")
+                product_name = product.find_element_by_class_name("product").text
+                product_id = product_url.split("/")[5].split("?")[0].replace(".html", "")
+                product_orders = int(product.find_element_by_css_selector("a.order-num-a em").text[8:].replace(")", ""))
+                self.products['iteration'] = self.products['iteration'] + 1
+                self.products[product_id] = {'name': product_name, 'url': product_url, 'orders': product_orders, 'us_orders': 0}
+                for index in range((product_orders // 8) if (product_orders % 8 == 0) else (product_orders // 8 + 1)):
+                    yield Request(url="https://feedback.aliexpress.com/display/evaluationProductDetailAjaxService.htm?productId=" + product_id + "&type=default&page=" + str(index + 1), callback=self.get_us_order, meta={'product_id': product_id})
 
+            # product_urls = response.css("a.product::attr(href)").extract()
+            #
+            # for product_url in product_urls:
+            #     product_url = product_url[2:]
+            #     product_id = product_url.split("/")[3].split("?")[0].replace(".html", "")
+            #     actual_url = "https://" + product_url
+            #     yield Request(url=actual_url, callback=self.get_product_details, meta={'product_id': product_id, 'actual_url': actual_url})
+
+    # def get_product_details(self, response):
+    #     if "login" in response.url:
+    #         self.driver.get(response.url)
+    #         validation_box = self.driver.find_element_by_id('alibaba-login-box')
+    #         self.driver.switch_to.frame(validation_box)
+    #         self.driver.find_element_by_id('has-login-submit').click()
+    #         actual_url = response.meta['actual_url']
+    #         yield Request(url=actual_url, callback=self.get_product_details, meta={'product_id': response.meta['product_id'], 'actual_url': actual_url}, dont_filter=True)
+    #     else:
+    #         product_id = response.meta['product_id']
+    #         self.products[product_id] = {}
+    #         self.products['iteration'] = self.products['iteration'] + 1
+    #         self.products[product_id]['name'] = response.css("h1.product-name::text").extract_first()
+    #         self.products[product_id]['url'] = response.url
+    #         self.products[product_id]['orders'] = int(response.css("span.order-num::text").extract_first().replace('orders', '').strip())
+    #         orders = int(self.products[product_id]['orders'])
+    #         self.products[product_id]['us_orders'] = 0
+    #         for index in range((orders // 8) if (orders % 8 == 0) else (orders // 8 + 1)):
+    #             yield Request(url="https://feedback.aliexpress.com/display/evaluationProductDetailAjaxService.htm?productId=" + product_id + "&type=default&page=" + str(index + 1), callback=self.get_us_order, meta={'product_id': product_id})
+    #
     def get_us_order(self, response):
-        us_orders = 0
+        product_id = response.meta['product_id']
         data = json.loads(response.body_as_unicode()) if json.loads(response.body_as_unicode()) else ""
         if(data != ""):
             for item in data['records']:
                 if(item['countryCode'] == 'us'):
-                    us_orders = self.us_orders + 1
-        return us_orders
+                    self.products[product_id]['us_orders'] = self.products[product_id]['us_orders'] + 1
+        if self.products['iteration'] > 45:
+            self.log(self.products)
